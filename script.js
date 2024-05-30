2b4fcff9c321a1a7ed712c073c89904894856a80
ptejalaxioned <tejalp@axioned.com>
2024-05-30 19:57:59 +0530

    JS correction file commit

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ var span=document.querySelector("span")
 
 function startBlur(){
     var count =parseInt(span.innerHTML)
-    let b=8
+    let b=100
   console.log(count)
     function incrementCount() {
         span.innerHTML=count
-        b=b-0.08;
+        b=b-1;
         img.style.filter=`blur(${b+"px"})`
-      count--;
-      if (count<1) {
-        loader.style.visibility="hidden"
-         img.style.filter="blur(0px)"
+      count++;
+      if (count>100) {
+        // loader.style.visibility="hidden"
+        //  img.style.filter="blur(0px)"
+        loader.classList.add("visibility")
+        img.classList.add("imageBlur")
         clearInterval(intervalId);
       }
     } 
-    var intervalId = setInterval(incrementCount, 10);
+    var intervalId = setInterval(incrementCount, 50);
   }
